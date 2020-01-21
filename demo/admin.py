48c1d3c190f392fe79dcbd5a1cade95c196e035d
alexxxstep <astep2004@gmail.com>
2020-01-21 18:59:20 +0200

    new commit

--- demo/admin.py
+++ demo/admin.py
@@ -4,6 +4,6 @@ from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description','published', 'price']
+    list_display = ['title', 'description', 'published', 'price']
     list_filter = ['published']
     search_fields = ['title', 'description']
--- demo/views.py
+++ demo/views.py
@@ -4,8 +4,7 @@ from django.views import View
 from .models import Book
 
 
-
 def first(request):
     books = Book.objects.all()
 
-    return render(request, 'f_temp.html',{'books': books})
+    return render(request, 'f_temp.html', {'books': books})
