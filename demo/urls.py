839434f5f294a007ec7efc08475521ab406d0d2b
alexxxstep <astep2004@gmail.com>
2020-01-21 22:06:28 +0200

    add new commit serialisers and postmn

--- demo/urls.py
+++ demo/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+from .views import BookVieSet
 
+router = routers.DefaultRouter()
+router.register('books', BookVieSet)
 
 urlpatterns = [
-    path('first', views.first),
+    path('', include(router.urls))
 
 
 ]
