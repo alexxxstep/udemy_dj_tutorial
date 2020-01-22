51c76091496290ab5e9138eca585c421b6f8eb9e
alexxxstep <astep2004@gmail.com>
2020-01-22 19:05:44 +0200

    add token and autorization

--- first/urls.py
+++ first/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
 
     path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
+    path('auth/', obtain_auth_token),
 
 ]
