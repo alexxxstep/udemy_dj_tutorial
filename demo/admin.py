04a893754190101693b7be0c0915b93d18ab5248
alexxxstep <astep2004@gmail.com>
2020-01-30 20:20:18 +0200

    add new models - one-to-one

--- demo/admin.py
+++ demo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, BookNumber
 
 
 @admin.register(Book)
@@ -7,3 +7,6 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'published', 'price']
     list_filter = ['published']
     search_fields = ['title', 'description']
+
+
+admin.site.register(BookNumber)
--- demo/models.py
+++ demo/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 # Create your models here.
+class BookNumber(models.Model):
+    isbn_10 = models.CharField(max_length=10, blank=True)
+    isbn_13 = models.CharField(max_length=13, blank=True)
+
+    def __str__(self):
+        return self.isbn_10
 
 class Book(models.Model):
     BOOKS = (('HB', 'Hobbit',),
@@ -21,8 +27,15 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='covers/', blank=True, default='')
     author = models.CharField(max_length=150, blank=True)
 
+    number = models.OneToOneField(BookNumber, null=True, blank=True, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
+
 class Author(models.Model):
     name = models.CharField(max_length=150, blank=False)
+
+
+
+
